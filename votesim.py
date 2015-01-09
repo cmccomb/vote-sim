b4edb5aabf0b1fa57a839c588f3ef21797c73e6e
cmccomb <ccmcc2012@gmail.com>
2015-01-09 09:33:30 -0500

    Super PEPpy

--- votesim.py
+++ votesim.py
@@ -1,14 +1,11 @@
-from numpy import dot, unique, max, inf, round, zeros, mean, ones, delete
-from numpy.random import shuffle, normal
-from random import choice, sample
-from itertools import permutations, combinations
-from scipy.io import loadmat
+import numpy as np
+from itertools import permutations
 
 
 # Finds maximum value in a dict, returns key
 def find_dict_max(some_dict):
     max_key = []
-    max_val = -inf
+    max_val = -np.inf
   
     for k, v in some_dict.iteritems():
         if v > max_val:
@@ -21,7 +18,7 @@ def find_dict_max(some_dict):
 # Finds minimum value in a dict, returns key
 def find_dict_min(some_dict):
     min_key = []
-    min_val = inf
+    min_val = np.inf
 
     for k, v in some_dict.iteritems():
         if v < min_val:
@@ -37,10 +34,10 @@ def sort_dict(some_dict, direction='ascending'):
 
     if direction == 'ascending':
         find_next = find_dict_min
-        reset = inf
+        reset = np.inf
     elif direction == 'descending':
         find_next = find_dict_max
-        reset = -inf
+        reset = -np.inf
     else:
         print("Please specify 'ascending' or 'descending'")
         return 0
@@ -55,7 +52,7 @@ def sort_dict(some_dict, direction='ascending'):
 
 
 ## Voting Functions
-def make_safe_profile(profile):
+def safe_copy(profile):
     prof = []
     for voter in profile[:]:
         temp = []
@@ -74,7 +71,7 @@ def remove_candidate(profile, candidate):
 
 def plurality(profile):
     # Make dictionary
-    candidates = dict.fromkeys(list(unique(profile[0])), 0)
+    candidates = dict.fromkeys(list(np.unique(profile[0])), 0)
 
     # Count votes
     for voter in profile:
@@ -88,7 +85,7 @@ def plurality(profile):
 
 def veto(profile):
     # Make dictionary
-    candidates = dict.fromkeys(list(unique(profile[0])), 0)
+    candidates = dict.fromkeys(list(np.unique(profile[0])), 0)
 
     # Count vetos
     for voter in profile:
@@ -102,7 +99,7 @@ def veto(profile):
 
 def borda(profile):
     # Make dictionary
-    candidates = dict.fromkeys(list(unique(profile[0])), 0)
+    candidates = dict.fromkeys(list(np.unique(profile[0])), 0)
 
     # Count score
     for voter in profile:
@@ -117,7 +114,7 @@ def borda(profile):
 
 def copeland(profile):
     # Make dictionary
-    candidates = dict.fromkeys(list(unique(profile[0])), 0)
+    candidates = dict.fromkeys(list(np.unique(profile[0])), 0)
     all_names = candidates.keys()
     m = len(all_names)
 
@@ -130,7 +127,7 @@ def copeland(profile):
             names.remove(all_names[j])
 
             # Make the new profile
-            prof = make_safe_profile(profile)
+            prof = safe_copy(profile)
             for name in names:
                 remove_candidate(prof, name)
 
@@ -149,7 +146,7 @@ def copeland(profile):
 def irv(profile):
 
     # Make new profile for safety
-    prof = make_safe_profile(profile)
+    prof = safe_copy(profile)
 
     # Get number of voters, alternatives
     m = len(prof[0])
@@ -178,169 +175,79 @@ RULES = [plurality, veto, borda, irv, copeland]
 
 
 ## Preference Profile
-class SubProfile(object):
+class PreferenceProfile(object):
 
-    def __init__(self, profile):
-        self.orig_profile = profile
-        self.n = len(self.orig_profile)
-        self.m = len(self.orig_profile[0])
-        self.names = list(unique(self.orig_profile[0]))
+    def __init__(self):
+        # Don't do very much
+        self.n = 0
+        self.m = 0
+        self.names = []
+        self.profile = []
+        self.prob = []
 
-    def make_reduced_profile(self, candidates):
-        # Figure out which to remove
-        to_remove = list(set(self.names) - set(candidates))
+    def make_random_profile(self, n, m):
+        # Store characteristics
+        self.m = m
+        self.n = n
 
-        # Make a new profile
-        self.temp_profile = make_safe_profile(self.orig_profile)
-        self.temp_names = candidates
+        # Make list of names
+        self.names = [format(i, '02d') for i in range(self.m)]
 
-        # Remove as appropriate
-        for cand in to_remove:
-            remove_candidate(self.temp_profile, cand)
+        # Get the profile
+        self.profile = []
+        for i in range(self.n):
+            xn = self.names[:]
+            np.random.shuffle(xn)
+            self.profile.append(xn)
 
-        # Make a probability matrix
-        m = len(self.temp_profile[0])
-        probs = zeros([m, m])
-        for voter in self.temp_profile:
-            for i, name1 in enumerate(self.temp_names):
-                for j, name2 in enumerate(self.temp_names):
+        # Make the probability comparison matrix
+        self.prob = np.zeros([self.m, self.m])
+        for voter in self.profile:
+            for i, name1 in enumerate(self.names):
+                for j, name2 in enumerate(self.names):
                     if voter.index(name1) > voter.index(name2):
-                        probs[i,j] += 1.0
+                        self.prob[i, j] += 1.0
 
-        self.temp_probs = probs/len(self.temp_profile)
+        self.prob /= len(self.profile)
 
     def strategyproof(self, rule):
-        # Get number of voters, alternatives
-        names = list(unique(self.temp_profile[0]))
-        m = len(names)
-        n = len(self.temp_profile)
-
         # Check ranking as is
-        sp, sc = rule(self.temp_profile)
+        sp, sc = rule(self.profile)
         winner = sp[0]
 
         # Create all permutations
-        options = permutations(names)
+        options = permutations(self.names)
 
         # Check all strategies
-        for i in range(n):
-            if self.temp_profile[i][0] != winner:
-                prof = make_safe_profile(self.temp_profile)
+        for i in range(self.n):
+            if self.profile[i][0] != winner:
+                prof = safe_copy(self.profile)
                 for strategy in options:
                     prof[i] = list(strategy)
                     sp, sc = rule(prof)
-                    if self.temp_profile[i].index(sp[0]) < \
-                            self.temp_profile[i].index(winner):
+                    if self.profile[i].index(sp[0]) < \
+                            self.profile[i].index(winner):
                         return False
         else:
             return True
 
     def unanimity(self, rule):
         # Check ranking as is
-        sp, _ = rule(self.temp_profile)
+        sp, _ = rule(self.profile)
 
-        for i, name1 in enumerate(self.temp_names):
-            for j, name2 in enumerate(self.temp_names):
-                if self.temp_probs[i, j] == 1.0:
+        for i, name1 in enumerate(self.names):
+            for j, name2 in enumerate(self.names):
+                if self.prob[i, j] == 1.0:
                     if sp.index(name1) < sp.index(name2):
                         return False
         else:
             return True
 
 
-class FullEmpiricalProfile(object):
+PP = PreferenceProfile()
+PP.make_random_profile(m=5, n=25)
+for x in PP.profile:
+    print(x)
 
-    def __init__(self, target, delt=0.1):
-        MAT = loadmat(target)
-
-        # This loads the predicted ratings, and sorts them.
-        ratings = MAT["predicted_ratings"].T
-        self.full_mat = MAT["full_mat"]
-        self.alpha_mean = MAT["alpha_mean"]
-        self.chol_cov = MAT["chol_cov"]
-        m = len(ratings[0])
-        names = [format(i, '02d') for i in range(m)]
-        profile = []
-        for voter in ratings:
-            temp = [(voter[i], names[i]) for i in range(m)]
-            temp2 = sorted(temp, reverse=True)
-            ranking = [temp2[i][1] for i in range(m)]
-            profile.append(ranking)
-
-        self.profile = profile
-        self.n = len(self.profile)
-        self.m = len(self.profile[0])
-        self.names = list(unique(self.profile[0]))
-
-        probs = delt*ones([self.m, self.m])
-        for voter in profile:
-            for i, name1 in enumerate(self.names):
-                for j, name2 in enumerate(self.names):
-                    if voter.index(name1) > voter.index(name2):
-                        probs[i, j] += 1.0
-
-        probs /= (self.n+delt)
-
-        self.probs = probs
-
-    def make_random_team(self, n_team, n_cand):
-        # Make the new profile
-        new_profile = []
-        for i in range(n_team):
-            x = self.names[:]
-            shuffle(x)
-            new_profile.append(x)
-
-        # Select some candidates
-        new_names = sample(self.names, n_cand)
-
-        p = SubProfile(new_profile)
-        p.make_reduced_profile(new_names)
-
-        return p
-
-    def make_empirical_team(self, n_team, n_cand):
-        # Make the new profile
-        new_profile = []
-        for _ in range(n_team):
-            j = choice(range(self.n))
-            new_profile.append(self.profile[j][:])
-
-        # Select some candidates
-        new_names = sample(self.names, n_cand)
-
-        p = SubProfile(new_profile)
-        p.make_reduced_profile(new_names)
-
-        return p
-
-    def make_empirical_team2(self, n_team, n_cand):
-        # Go through and generate set of ratings
-        all_ratings = []
-        for i in range(n_team):
-            temp = normal(size=7)
-            alpha = self.alpha_mean + dot(self.chol_cov, temp)
-            rating = dot(self.full_mat, alpha.T)
-            all_ratings.append(rating)
-
-        # From ratings, generate rankings
-        m = len(all_ratings[0])
-        names = [format(i, '02d') for i in range(m)]
-        profile = []
-        for voter in all_ratings:
-            temp = [(voter[i], names[i]) for i in range(m)]
-            temp2 = sorted(temp, reverse=True)
-            ranking = [temp2[i][1] for i in range(m)]
-            profile.append(ranking)
-
-        # Select some candidates
-        new_names = sample(self.names, n_cand)
-
-        p = SubProfile(profile)
-        p.make_reduced_profile(new_names)
-
-        return p
-
-
-
-
+print(copeland(PP.profile))
+print(PP.unanimity(copeland))
