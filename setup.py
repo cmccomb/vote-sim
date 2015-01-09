5880b82680b12db2afcfade887526549ca10dadb
Chris McComb <ccmcc2012@gmail.com>
2015-01-08 20:04:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
+# -*- coding: utf-8 -*-
 
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='votesim',
+    version='0.0.1',
+    description='Voting simulation and analysis',
+    long_description=readme,
+    author='Christopher McComb',
+    author_email='chris.c.mccomb@gmail.com',
+    url='https://github.com/cmccomb/voteSim',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
